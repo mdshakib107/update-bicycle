306380b64c1cf37848ee70390165974f4f13e2cd
mdshakib107 <mdshakib105100@gmail.com>
2025-04-09 22:58:49 +0600

    RTK Quary Data loding done

--- src/pages/allBicycles/AllBicycles.tsx
+++ src/pages/allBicycles/AllBicycles.tsx
@@ -1,11 +1,14 @@
+import { useGetAllProductsQuery } from "@/redux/api/baseApi";
 import Navbar from "../../components/home/Navbar";
 
 const AllBicycles = () => {
+  const { data, isLoading, isError } = useGetAllProductsQuery(undefined);
+  //   console.log(data);
+
   return (
     <div>
       <div className="container mx-auto min-h-screen space-y-6 sm:space-y-8 lg:space-y-12 px-4 sm:px-6 lg:px-8">
         <Navbar />
-        <div className="min-h-[30vh] sm:min-h-[40vh] my-6 sm:my-8 flex flex-col justify-center items-center gap-8 sm:gap-12 lg:gap-16"></div>
         all Bicyckes
       </div>
     </div>
