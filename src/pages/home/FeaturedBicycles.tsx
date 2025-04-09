a981a88c0547bd382d258a779ba412cc3559ffa5
mdshakib107 <mdshakib105100@gmail.com>
2025-04-10 00:40:35 +0600

    all bicycles page setup done

--- src/pages/home/FeaturedBicycles.tsx
+++ src/pages/home/FeaturedBicycles.tsx
@@ -1,14 +1,13 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { useQuery } from "@tanstack/react-query";
+import { useNavigate } from "react-router-dom";
+import { toast } from "sonner";
+import CustomButton from "../../components/shared/CustomButton";
 import ItemsCard, { ItemData } from "../../components/shared/ItemsCard";
 import Loading from "../../components/shared/Loading";
-import { toast } from "sonner";
 import useAxiosCommon from "../../hooks/useAxiosCommon";
-import CustomButton from "../../components/shared/CustomButton";
-import { useNavigate } from "react-router-dom";
 
 const FeaturedBicycles = () => {
-
   // axios hook
   const axiosCommon = useAxiosCommon();
 
@@ -42,7 +41,7 @@ const FeaturedBicycles = () => {
 
   // handle click to navigate to #featured
   const handleClick = () => {
-    navigate('')
+    navigate("/AllBicycles");
     toast.success("Navigating to Featured Section!");
   };
 
@@ -57,9 +56,8 @@ const FeaturedBicycles = () => {
     <div className="w-full min-h-[45vh] sm:min-h-[55vh] lg:min-h-[60vh] rounded-4xl shadow-purple-600 shadow-2xl p-6 sm:p-8 md:p-12 lg:p-16">
       {/* header */}
       <header className="flex h-full flex-col gap-12 lg:gap-0 lg:flex-row justify-center items-center lg:mt-3">
-      <div className="px-6 sm:px-8 mt-8 lg:mt-0 w-full lg:w-[50%] space-y-6">
-
-      <h1 className="text-[32px] sm:text-[40px] lg:text-[60px] leading-[40px] sm:leading-[45px] lg:leading-[65px] font-[500] w-full">
+        <div className="px-6 sm:px-8 mt-8 lg:mt-0 w-full lg:w-[50%] space-y-6">
+          <h1 className="text-[32px] sm:text-[40px] lg:text-[60px] leading-[40px] sm:leading-[45px] lg:leading-[65px] font-[500] w-full text-center">
             Featured Bicycles
           </h1>
 
@@ -73,12 +71,14 @@ const FeaturedBicycles = () => {
               handleAnything={handleClick}
             />
           </div>
-
         </div>
       </header>
 
       {/* grid */}
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3 gap-8 px-4 sm:px-6 lg:px-8 mt-10" id="featured">
+      <div
+        className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3 gap-8 px-4 sm:px-6 lg:px-8 mt-10"
+        id="featured"
+      >
         {data?.map((d: ItemData) => (
           <ItemsCard key={d._id} data={d} isPending={isPending} />
         ))}
