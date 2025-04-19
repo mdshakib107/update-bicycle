b8a8577f35df2b90c6333c010121e8f5c6bab048
MarGoN <monondcosta@gmail.com>
2025-04-19 19:27:31 +0600

    Fix: fixed !inStock bicycles will not be shown in slider & features and will not navigate to checkoutpage

--- src/pages/checkout/Checkout.tsx
+++ src/pages/checkout/Checkout.tsx
@@ -3,19 +3,35 @@ import Loading from "../../components/shared/Loading";
 import useAxiosCommon from "../../hooks/useAxiosCommon";
 import { useQuery } from "@tanstack/react-query";
 import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import { toast } from "sonner";
 import { useForm } from "react-hook-form";
 import ResponsiveNavbar from "@/components/home/ResponsiveNavbar";
 import CustomButton from "@/components/shared/CustomButton";
+import { useGetProductByIdQuery } from "@/redux/api/productApi";
 
 const Checkout = () => {
   const axiosCommon = useAxiosCommon();
   const { id } = useParams();
   const [totalPrice, setTotalPrice] = useState(0);
   const [userId, setUserId] = useState("");
-  
 
+  // navigation
+  const navigate = useNavigate();
+
+  // check data for stock
+  const { data, isLoading, isError } = useGetProductByIdQuery(id as string);
+
+  const productData = data?.data;
+
+  useEffect(() => {   
+
+    if (!productData?.inStock) {
+      toast.info("Items is not available!");
+
+      navigate("/");
+    }
+  }, [productData, navigate]);
 
   useEffect(() => {
     const localUser = localStorage.getItem("userData");
@@ -24,28 +40,6 @@ const Checkout = () => {
       setUserId(user?._id);
     }
   }, []);
-
-  // type TStatus =
-  //   | "PENDING"
-  //   | "PROCESSING"
-  //   | "ON THE WAY"
-  //   | "DELIVERED"
-  //   | "CANCELED";
-
-  // type TOrderedItem = {
-  //   product: string;
-  //   quantity: number;
-  //   price: number;
-  // };
-
-  // type TOrder = {
-  //   products: TOrderedItem[];
-  //   user: string;
-  //   totalPrice: number;
-  //   isDeleted?: boolean;
-  //   status: TStatus;
-  //   paymentStatus: "UNPAID";
-  // };
 
   const {
     register,
@@ -71,7 +65,7 @@ const Checkout = () => {
         }
 
         // return the featured Bicycles
-        return response.data.data; 
+        return response.data.data;
       } catch (error: any) {
         console.error("Error fetching featured bicycles:", error);
 
@@ -115,16 +109,19 @@ const Checkout = () => {
 
     const response = await axiosCommon.post(
       "/api/orders/create-order",
-      orderData
+      orderData,
     );
     console.log(response);
     window.location.replace(response.data.data.GatewayPageURL);
     console.log(response.data.data);
-
   };
 
   if (isPending) return <Loading />;
   // console.log(product);
+
+  if (isError) return toast.error("Something went wrong!");
+
+  if (isLoading) return <Loading />;
 
   return (
     <div className="min-h-screen container mx-auto space-y-6 sm:space-y-8 lg:space-y-12 sm:px-6 px-4 lg:px-8">
@@ -182,11 +179,11 @@ const Checkout = () => {
                   <h4 className="text-lg font-semibold">Order Summary</h4>
                   <div className="flex justify-between mt-2">
                     <span>Subtotal ({quantity} items):</span>
-                    <span>Tk{totalPrice}</span>
+                    <span>{totalPrice} Taka</span>
                   </div>
                   <div className="border-t mt-2 pt-2 font-bold flex justify-between">
                     <span>Total:</span>
-                    <span>Tk{totalPrice}</span>
+                    <span>{totalPrice} Taka</span>
                   </div>
                 </div>
 
