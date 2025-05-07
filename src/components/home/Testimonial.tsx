d11a2e5133d7fec7f89faeef859d42ed277e5dd5
Noc1 PC MargoN <monondcosta@gmail.com>
2025-05-07 16:30:55 +0600

    Modify: small adjustments to testimonials.

--- src/components/home/Testimonial.tsx
+++ src/components/home/Testimonial.tsx
@@ -35,18 +35,18 @@ const Testimonial = () => {
   ];
 
   return (
-    <div className="w-full h-[85vh] xl:h-[60vh] rounded-4xl! shadow-purple-600 shadow-2xl">
+    <div className="w-full h-[85vh] xl:h-[70vh] rounded-4xl! shadow-purple-600 shadow-2xl">
       <Carousel
         autoplay
         autoplaySpeed={2000}
         arrows
         fade
-        className="min-w-full h-[85vh] xl:h-[60vh] bg-gradient-to-r from-blue-400 to-purple-600 rounded-4xl"
+        className="min-w-full h-[85vh] xl:h-[70vh] bg-gradient-to-r from-blue-400 to-purple-600 rounded-4xl"
       >
         {testimonials.map((testimonial, index) => (
           <section
             key={index}
-            className="relative isolate overflow-hidden bg-transparent px-6 py-24 sm:py-32 lg:px-8 rounded-4xl w-full h-[85vh] xl:h-[60vh]! "
+            className="relative isolate overflow-hidden bg-transparent px-6 py-16 lg:px-8 rounded-4xl w-full h-[85vh] xl:h-[70vh]! "
           >
             <div className="absolute inset-0 -z-10 bg-[radial-gradient(45rem_50rem_at_top,var(--color-indigo-100),white)] opacity-20"></div>
             <div className="absolute inset-y-0 right-1/2 -z-10 mr-16 w-[200%] origin-bottom-left skew-x-[-30deg] bg-white ring-1 shadow-xl shadow-indigo-600/10 ring-indigo-50 sm:mr-28 lg:mr-0 xl:mr-16 xl:origin-center"></div>
