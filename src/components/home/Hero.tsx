778f74ec4a1215aa68d47ce5db2a295e915e3cce
MarGoN <monondcosta@gmail.com>
2025-05-07 10:13:21 +0600

    Fix: Fixed the hero section spacing

--- src/components/home/Hero.tsx
+++ src/components/home/Hero.tsx
@@ -14,13 +14,13 @@ const Hero = () => {
   return (
     <div className="w-full rounded-4xl shadow-purple-600 shadow-2xl flex flex-col justify-center items-center">
       {/* header */}
-      <header className="flex h-full lg:flex-row-reverse flex-col-reverse gap-4 lg:gap-0 justify-center items-center ">
-        <div className="px-8 mt-0  w-full lg:w-[60%] h-full py-4">
-          <div className="flex flex-col items-center justify-center text-center space-y-6">
-            <h1 className="text-4xl sm:text-5xl lg:text-6xl font-bold tracking-tight transition-all duration-300 hover:scale-[1.02]">
-              Discover Your Perfect Ride
+      <header className="flex h-full lg:flex-row flex-col-reverse gap-4 lg:gap-0 justify-center items-center ">
+        <div className="px-8 mt-0  w-full lg:w-[60%] h-full py-4 lg:py-0 lg:h-80">
+          <div className="flex flex-col items-center justify-start text-center space-y-8">
+            <h1 className="text-4xl md:text-5xl font-bold tracking-tight transition-all duration-300 hover:scale-[1.02]">
+              Discover Your Perfect Ride With Us!
             </h1>
-            <p className="max-w-2xl text-lg md:text-xl text-gray-600 leading-tight text-justify lg:text-center">
+            <p className="max-w-2xl text-lg md:text-xl text-gray-600 leading-relaxed text-justify lg:text-center">
               Explore our premium collection of bicycles, crafted for
               performance and style. Find your ideal companion for every
               journey.
